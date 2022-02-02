4041de9698b74e5a83f35c59e424dcd55e4b6aa8
weather <kisangjan9.naver.com>
2022-02-02 09:57:35 +0900

    no~

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import LoginPage from "./components/views/LoginPage/Index";
 
 ReactDOM.render(
-  
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
